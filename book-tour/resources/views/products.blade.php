724e9673ba6802b78a83b9909b801f3beb06d388
SafiyyahEhigie <safiyyahehigie@gmail.com>
2023-11-24 13:02:09 +0000

    Front-end first draft for products page
    Added a html, css and javascript file for the products page

---
+++ book-tour/resources/views/products.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookTour</title>
+    <!-- font link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <!-- css file -->
+    <link rel="stylesheet" type="text/css" href="/css/products.css"/>
+</head>
+<body>
+    <!-- header -->
+    <header class="header">
+        <div class="header-1">
+            <img src="/images/logo.png" alt="Your Logo" class="logo-image">
+            <a href="#" class="logo"> </i> BookTour</a>
+
+            <form action="" class="search-form">
+            <input type="search" name="" placeholder="what's your next adventure? search here..." id="search-box">
+            <label for="search-box" class="fas fa-search"></label>
+        </form>
+
+        <div class="icons">
+            <div id="search-btn" class="fas fa-search"></div>
+            <a href="#" class="fas fa-heart"></a>
+            <a href="#" class="fas fa-shopping-cart"></a>
+            <div id="login-btn" class="fas fa-user"></div>
+        </div>
+    </div>
+    <div class="header-2">
+        <nav class="navbar">
+            <a href="#Home">Home</a>
+            <a href="#Featured">Featured</a>
+            <a href="#New Arrivals">New Arrivals</a>
+            <a href="#Best Sellers">Best Sellers</a>
+            <a href="#Genres">Genres</a>
+            <a href="#E-Books">E-Books</a>
+            <a href="#Special Offers">Special Offers</a>
+            <a href="#Reviews">Reviews</a>
+            <a href="#Blogs">Blogs</a>
+
+        </nav>
+    </div>
+
+    </header>
+    <!-- header -->
+    
+    <!-- lower navbar -->
+    <nav class="lower-navbar">
+        <a href="#Home" class="fas fa-home"></a>
+        <a href="#Featured" class="fas fa-list"></a>
+        <a href="#New Arrivals" class="fas fa-tags"></a>
+        <a href="#Best Sellers" class="fas fa-heart"></a>
+        <a href="#Special Offers" class="fas fa-user"></a>
+        <a href="#Reviews" class="fas fa-comments"></a>
+
+    </nav>
+    <!-- login -->
+
+
+    <!-- js file -->
+    <script src="/js/products.js">
+    </script>
+</body>
