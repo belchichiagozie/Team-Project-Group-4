f389f1e68536d6ac3d1623a920ea63d0c94a6848
Safa Husnat <safa.husnat@icloud.com>
2023-11-28 12:37:49 +0000

    modified:   book-tour/composer.json 	modified:   book-tour/composer.lock 	modified:   book-tour/package.json 	modified:   book-tour/resources/js/app.js 	modified:   book-tour/resources/js/bootstrap.js 	modified:   book-tour/routes/web.php 	modified:   book-tour/vite.config.js

--- book-tour/routes/web.php
+++ book-tour/routes/web.php
@@ -24,6 +24,32 @@ Route::get('/', function () {
 Route::get('/products', function () {
     return view('/products');
 });
+<?php
+
+use App\Http\Controllers\AdminProductController;
+use App\Http\Controllers\AdminCustomerController;
+use App\Http\Controllers\AdminOrderController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('/products');
+});
+
+Route::get('/products', function () {
+    return view('/products');
+});
 
 Route::get('/admin/dashboard', [AdminController::class, 'index']);
 Route::redirect('admin', 'admin/dashboard');
