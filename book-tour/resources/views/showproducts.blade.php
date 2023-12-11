5676c7cfb4e2c80ed61fd7f4429227af6aab0ada
zahidf <zahidfaqiri786@outlook.com>
2023-12-11 13:34:18 +0000

    Pushing on behalf of Ashley Student no 210177260
    Worked on showproducts view to show each individual book item

--- book-tour/resources/views/showproducts.blade.php
+++ book-tour/resources/views/showproducts.blade.php
@@ -1,30 +1,26 @@
 @extends('layouts.navbar')
 
 @section('content')
-    <div class="books-slider">
-        <div class="wrapper">
-            <a href="#"><img src="/images/{{$book->file}}" alt="{{$book->Book_ID}}"></a>
+<div class="image-details">
+    <div class="left-side">
+        <div class="product-image">
+            <img src="/images/{{$book->file}}" alt="{{$book->Book_ID}}" width="300px" height="380px">
         </div>
     </div>
-
-    <div class="reviews-section">
-        <button>
-            <a href="{{ url('add-review/'.$book->Book_ID) }}" class="reviews-button">Write a Review</a>
-        </button>
-
-        <div class="reviews-body">
-            <h3 for="reviews">Reviews</h3>
-            <p>This is a review</p>
+    <div class="right-side">
+        <div class="details-top">
+            <div class="name" id="name">[Name]</div> <!-- Placeholder for user's name -->
+            <div class="book-name" id="book-name">{{$book->Title}}</div>
         </div>
-
+        <div class="details-middle">
+            <div class="book-genre" id="book-genre">Book Genre: {{$book->Genre}}</div>
+            <div class="book-author">The author of this book is {{$book->Author}}</div>
+            <div class="stock-amount">We have {{$book->Stock}} in stock!</div>
+        </div>
+        <div class="details-bottom">
+            <button class="add-to-favorites">Add to Favorites</button>
+            <button class="add-to-basket">Add to Basket</button>
+        </div>
     </div>
-
-
-    <!-- js file -->
-    <script src="/js/products.js">
-    </script>
-</body>
-
-<!-- swiper -->
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+</div>
 @endsection
