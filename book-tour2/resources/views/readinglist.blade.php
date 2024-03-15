3998b1a5517236b953a4da54827eb3c3a4bb3322
Safa Husnat <safa.husnat@icloud.com>
2024-03-15 16:13:44 +0000

    readinglist front end

---
+++ book-tour2/resources/views/readinglist.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.navbar')
+
+@section('content')
+<section class="readinglist">
+<body>
+    <div class="heading">
+        <h1>Reading List</h1>
+            <div class="add to basket">
+            <div class="box">
+                <img src="/images/bookcover4.png" alt="game of your mind">
+                <div class="list">
+                <h2>Game Of Your Mind</h2>
+                <h3>Price: £10</h3>
+                <p class="addbtn">
+                            <span class="btn1">Add to basket</span>
+                        </p>
+                        <p class="likebtn">
+                        <i class="fas fa-heart"></i>
+    <span class="btn2"></span>
+</p>
+</div>
+</div>
+    </div>
+<div class="box">
+    <img src="/images/bookcover5.png" alt="the spirit">
+    <div class="list">
+                <h2>The Spirit</h2>
+                <h3>Price: £10</h3>
+                <p class="addbtn">
+                            <span class="btn1">Add to basket</span>
+                        </p>
+                        <p class="likebtn">
+                        <i class="fas fa-heart"></i>
+    <span class="btn2"></span>
+</p>
+</div></div>
+    <div class="box">
+    <img src="/images/bookcover7.png" alt="robotics">
+    <div class="list">
+                <h2>Robotics</h2>
+                <h3>Price: £10</h3>
+                <p class="addbtn">
+                            <span class="btn1">Add to basket</span>
+                        </p>
+                        <p class="likebtn">
+                        <i class="fas fa-heart"></i>
+    <span class="btn2"></span>
+</p>
+</div>
+</div>
+    
+
+
+
+@endsection
